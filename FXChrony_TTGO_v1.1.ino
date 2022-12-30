6296c87f53d5ee91127cf64d33e432a6df276327
daystaterebel@gmail.com <daystaterebel@gmail.com>
2022-12-30 12:08:34 -0500

    Shot string stats

--- FXChrony_TTGO_v1.1.ino
+++ FXChrony_TTGO_v1.1.ino
@@ -12,7 +12,8 @@
 TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
 OpenFontRender render;
 
-#define EEPROM_SIZE 6
+#define SHOT_STRING_LENGTH  20
+#define EEPROM_SIZE (6 + (NUM_GUNS * (SHOT_STRING_LENGTH * sizeof(float) + 1)))
 
 static esp_adc_cal_characteristics_t adc_chars;
 
@@ -32,6 +33,7 @@ OneButton button(PIN_INPUT, true);
 #define STATE_IDLE          0
 #define STATE_CONNECTING    1
 #define STATE_CONNECTED     2
+#define STATE_OFFLINE       3
 
 #define seconds() (millis()/1000)
 
@@ -76,12 +78,18 @@ static uint8_t power_save_duration = 0;
 static uint8_t pellet_index = 0;
 static uint8_t gun_index = 0;
 
-static uint32_t shot_count = 0;
-
 static uint8_t nc_counter = 0;
 
 typedef  void (* menuItemCallback_t)(uint8_t);
 typedef  void (* menuItemGenString_t)(uint8_t, char *);
+
+typedef struct shot_stats {
+  float min;
+  float max;
+  float avg;
+  float es;
+  float sd;
+} shot_stats_t;
 
 typedef struct menuItem {
     const char * menuString;
@@ -100,9 +108,26 @@ static menuItem_t * menuStack[4];
 static int menuStackIndex;
 static menuItem_t * pCurrentMenuItem;
 
+class MyClientCallback : public BLEClientCallbacks {
+  void onConnect(BLEClient* pclient) {
+    Serial.println("onConnect");
+  }
+
+  void onDisconnect(BLEClient* pclient) {
+    Serial.println("onDisconnect");
+    /* this make me sad, can't get the code to reliably reconnect 
+    more than 4-5 times so here it is ... 
+    (On the plus side it seems to work well and I can get 
+    on with shot strings :) )*/
+    ESP.restart();
+    while(1){}
+  }
+};
+
+
 void setup() {
   Serial.begin(115200);
-  Serial.println("Starting Open Display (TTGO T Display v1.1) V1.1...");
+  Serial.printf("Starting Open Display (TTGO T Display v1.1) V1.1... %d EEPROM bytes\n",EEPROM_SIZE);
 
   button.attachDoubleClick(doubleClick);
   button.attachLongPressStop(longPressStop);
@@ -118,6 +143,7 @@ void setup() {
  * Read the Settings, if any setting is out of range
  * give it a sensible default value (cope with first use)
  */
+  
   EEPROM.begin(EEPROM_SIZE);
   sensitivity = EEPROM.read(0);
   if(sensitivity > SENSITIVITY_MAX) {
@@ -140,8 +166,8 @@ void setup() {
     EEPROM.write(3, display_flip);
   }
   power_save_duration = EEPROM.read(4);
-  if(power_save_duration > 10) {
-    power_save_duration = 10;
+  if(power_save_duration > 60) {
+    power_save_duration = 60;
     EEPROM.write(4, power_save_duration);
   }
   pellet_index = EEPROM.read(5);
@@ -174,6 +200,33 @@ void setup() {
   tft.fillScreen(TFT_BLACK);
   dirty = true;
 }
+
+uint8_t get_string_length(uint8_t gidx) {
+  return EEPROM.read(6 + (gidx * (SHOT_STRING_LENGTH * sizeof(float) + 1)));
+}
+
+void clear_string(uint8_t gidx) {
+  EEPROM.write(6 + (gidx * (SHOT_STRING_LENGTH * sizeof(float) + 1)), 0);
+  EEPROM.commit();
+}
+
+float get_shot(uint8_t gidx, uint8_t sidx) {
+  float result;
+  EEPROM.get(6 + (gidx * (SHOT_STRING_LENGTH * sizeof(float) + 1)) + sidx * sizeof(float) + 1, result);
+  return result;
+}
+
+bool add_shot(uint8_t gidx, float speed) {
+  uint8_t sidx = get_string_length(gidx);
+  if(sidx < SHOT_STRING_LENGTH) {
+    EEPROM.put(6 + (gidx * (SHOT_STRING_LENGTH * sizeof(float) + 1)) + sidx * sizeof(float) + 1, speed);
+    EEPROM.write(6 + (gidx * (SHOT_STRING_LENGTH * sizeof(float) + 1)), sidx + 1);
+    EEPROM.commit();
+    return true;
+  }
+  return false;
+}
+
 
 void doRenderMenu() {
   char genHeader[256];
@@ -269,7 +322,7 @@ void do_scan() {
   pBLEScan->setInterval(1349);
   pBLEScan->setWindow(449);
   pBLEScan->setActiveScan(true);
-  pBLEScan->start(5, false);
+  pBLEScan->start(1, false);
 }
 
 bool writeChar(BLERemoteService* pRemoteService, int idx, uint8_t value)
@@ -364,18 +417,6 @@ bool readBattery(){
 }
 
 
-class MyClientCallback : public BLEClientCallbacks {
-  void onConnect(BLEClient* pclient) {
-  }
-
-  void onDisconnect(BLEClient* pclient) {
-    state = STATE_IDLE;
-    dirty = true;
-    renderMenu = false;
-  }
-};
-
-
 static void notifyCallback(
   BLERemoteCharacteristic* pBLERemoteCharacteristic,
   uint8_t* pData,
@@ -384,13 +425,13 @@ static void notifyCallback(
   char sbuffer[256];
   // Third byte is return in steps of 5%, anything better than 10% we display  
   uint8_t r = ((char*)pData)[2] * 5;
-  uint16_t speed;    
+  uint16_t speed;
+  uint16_t shot_color;
   if((r >= sensitivity) && !renderMenu) {
     display_on_at = seconds();
     power_saving = false;
 
     renderMenu = false;
-    shot_count++;    
 
     speed = ((char*)pData)[0];
     speed <<= 8;
@@ -444,11 +485,16 @@ static void notifyCallback(
 
     render.setFontSize(15);
     
-    sprintf (sbuffer, "Shot# %d", shot_count);
+    shot_color = TFT_WHITE;
+    if(!add_shot(gun_index, fspeed)) {
+      shot_color = TFT_RED;
+    }
+
+    sprintf (sbuffer, "Shot# %d/%d", get_string_length(gun_index), SHOT_STRING_LENGTH);
     render.drawString(sbuffer,
                 0,
                 120,
-                TFT_WHITE,
+                shot_color,
                 TFT_BLACK,
                 Layout::Horizontal);
 
@@ -473,7 +519,7 @@ void connectToChrony() {
   pClient  = BLEDevice::createClient();
   pClient->setClientCallbacks(new MyClientCallback());
   // Connect to the remove BLE Server.
-  pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+  pClient->connect(myDevice);
 
   // Obtain a reference to the service we are after in the remote BLE server.
   pRemoteService = pClient->getService(serviceUUID);
@@ -553,10 +599,12 @@ void loop() {
   switch(state)
   {
     case STATE_IDLE:
-      if(!dirty) { 
+//      if(!dirty) { 
         renderSearching();
         do_scan(); 
-      }
+//      }
+      break;
+    case STATE_OFFLINE:
       break;
     case STATE_CONNECTING:
       connectToChrony();
@@ -591,7 +639,7 @@ void menuItemGenStringCurSleep(uint8_t, char * buffer)
   sprintf(buffer, "[%s]", menu_sleep[0].menuString);
 }
 
-static const uint8_t power_save_duration_lut[] = {0,2,5,10};
+static const uint8_t power_save_duration_lut[] = {0,2,5,10,20,40,60};
 
 static void powerSaveCallback(uint8_t param)
 {
@@ -607,7 +655,10 @@ static menuItem_t menu_power_save[] = {
   { "Off",  NULL, &menu_power_save[1], NULL, NULL, powerSaveCallback, 0, NULL},
   { "2s",   NULL, &menu_power_save[2], NULL, NULL, powerSaveCallback, 1, NULL},
   { "5s",   NULL, &menu_power_save[3], NULL, NULL, powerSaveCallback, 2, NULL},
-  { "10s",  NULL, &menu_power_save[0], NULL, NULL, powerSaveCallback, 3, NULL}
+  { "10s",  NULL, &menu_power_save[0], NULL, NULL, powerSaveCallback, 3, NULL},
+  { "20s",  NULL, &menu_power_save[0], NULL, NULL, powerSaveCallback, 4, NULL},
+  { "40s",  NULL, &menu_power_save[0], NULL, NULL, powerSaveCallback, 5, NULL},
+  { "60s",  NULL, &menu_power_save[0], NULL, NULL, powerSaveCallback, 6, NULL},
 };
 
 void menuItemGenStringCurPowerSaving(uint8_t, char * buffer)
@@ -618,6 +669,9 @@ void menuItemGenStringCurPowerSaving(uint8_t, char * buffer)
     case 2:  idx = 1;  break;
     case 5:  idx = 2;  break;
     case 10: idx = 3;  break;
+    case 20: idx = 4;  break;
+    case 40: idx = 5;  break;
+    case 60: idx = 6;  break;
   }  
   sprintf(buffer, "[%s]", menu_power_save[idx].menuString);
 }
@@ -713,7 +767,6 @@ void menuItemGenStringPellet(uint8_t i, char * buffer)
 void menuItemGenStringCurSelPellet(uint8_t i, char * buffer)
 {
   sprintf(buffer, "[%s]",my_pellets[pellet_index].pellet_name);
-  Serial.println(buffer);  
 }
 
 static void selectGunCallback(uint8_t param)
@@ -742,13 +795,155 @@ void menuItemGenStringCurSelGun(uint8_t i, char * buffer)
   sprintf(buffer, "[%s]",my_guns[gun_index].gun_name);
 }
 
+void shotStringStats(shot_stats_t * ss)
+{
+  float speed, dist, sum = 0, sum_of_dists_sqd = 0;
+  uint8_t i, scnt = get_string_length(gun_index);
+  if(scnt == 0) {
+    ss->min = 0;
+    ss->max = 0;
+    ss->avg = 0;
+    ss->es = 0;
+    ss->sd = 0;
+    return;    
+  }
+
+  ss->min = 1000000;
+  ss->max = 0;
+
+  for(i = 0; i < scnt; i++) {
+    speed = get_shot(gun_index, i);
+    if(speed < ss->min){
+      ss->min = speed;
+    }
+    if(speed > ss->max){
+      ss->max = speed;
+    }
+    sum += speed;
+  }
+
+  ss->avg = sum / (float)scnt;
+  ss->es = ss->max - ss->min;
+
+  for(i = 0; i < scnt; i++) {
+    speed = get_shot(gun_index, i);
+    if(speed > ss->avg) {
+      dist = (speed - ss->avg);
+    } else {
+      dist = (ss->avg - speed);
+    }
+    sum_of_dists_sqd += powf(dist, 2);
+  }
+  ss->sd = sqrtf(sum_of_dists_sqd / (float)scnt);
+}
+
+static void shotStringClearCallback(uint8_t param)
+{
+  clear_string(gun_index);
+}
+
+static void shotStringDumpCallback(uint8_t param)
+{
+  shot_stats_t stats;
+  uint8_t i, scnt = get_string_length(gun_index);
+  pCurrentMenuItem = menuStack[--menuStackIndex];
+  Serial.printf("Shot String\n");
+  Serial.printf("Gun %s %s\n",my_guns[gun_index].gun_mfr, my_guns[gun_index].gun_name);
+  if(units == UNITS_IMPERIAL) {
+    Serial.printf("Ammo %s %.3f %.3f\n",my_pellets[pellet_index].pellet_name, my_pellets[pellet_index].pellet_caliber_inch, my_pellets[pellet_index].pellet_weight_grains);
+  } else {
+    Serial.printf("Ammo %s %.3f %.3f\n",my_pellets[pellet_index].pellet_name, my_pellets[pellet_index].pellet_caliber_mm, my_pellets[pellet_index].pellet_weight_grams);
+  }
+
+  shotStringStats(&stats);  
+  Serial.printf("Minimum : %.2f\n",stats.min);
+  Serial.printf("Maximum : %.2f\n",stats.max);
+  Serial.printf("Average : %.2f\n",stats.avg);
+  Serial.printf("ES      : %.2f\n",stats.es);
+  Serial.printf("SD      : %.2f\n",stats.sd);
+
+  for(i = 0; i < scnt; i++) {
+    Serial.printf("Shot %d: %.2f\n", i+1, get_shot(gun_index, i));
+  }
+}
+
+static void shotStringInitCallback(uint8_t param)
+{
+  uint8_t i;
+  for(i = 0; i < NUM_GUNS; i++) {
+    clear_string(i);
+  }
+  pCurrentMenuItem = menuStack[--menuStackIndex];
+}
+
+static void shotStringStatsCallback(uint8_t param)
+{
+  shot_stats_t stats;
+  char sbuffer[128];
+  shotStringStats(&stats);  
+  tft.fillScreen(TFT_BLACK);
+  render.setFontSize(20);
+
+  sprintf(sbuffer, "Min : %d\n",(int)stats.min);
+  render.cdrawString(sbuffer,
+              tft.width()/2,
+              5,
+              TFT_WHITE,
+              TFT_BLACK,
+              Layout::Horizontal);
+
+  sprintf(sbuffer, "Max : %d\n",(int)stats.max);
+  render.cdrawString(sbuffer,
+              tft.width()/2,
+              28,
+              TFT_WHITE,
+              TFT_BLACK,
+              Layout::Horizontal);
+  sprintf(sbuffer, "Ave : %d\n",(int)stats.avg);
+  render.cdrawString(sbuffer,
+              tft.width()/2,
+              51,
+              TFT_WHITE,
+              TFT_BLACK,
+              Layout::Horizontal);
+  sprintf(sbuffer, "ES  : %.2f\n",stats.es);
+  render.cdrawString(sbuffer,
+              tft.width()/2,
+              74,
+              TFT_WHITE,
+              TFT_BLACK,
+              Layout::Horizontal);
+  sprintf(sbuffer, "SD  : %.2f\n",stats.sd);
+  render.cdrawString(sbuffer,
+              tft.width()/2,
+              97,
+              TFT_WHITE,
+              TFT_BLACK,
+              Layout::Horizontal);
+  sleep(5);
+}
+
+static menuItem_t menu_shot_string[] = {
+  { "Stats",       NULL, &menu_shot_string[1], NULL, NULL, shotStringStatsCallback, 0, NULL},
+  { "Clear",       NULL, &menu_shot_string[2], NULL, NULL, shotStringClearCallback, 0, NULL},
+  { "Dump",        NULL, &menu_shot_string[3], NULL, NULL, shotStringDumpCallback, 0, NULL},
+  { "Initialise",  NULL, &menu_shot_string[0], NULL, NULL, shotStringInitCallback, 0, NULL}
+};
+
+void menuItemGenStringCurShotString(uint8_t, char * buffer)
+{
+  sprintf(buffer, "[%d]", get_string_length(gun_index));
+}
+
+
 static menuItem_t menu_top_level[] = {
   { "Gun",          NULL, &menu_top_level[1], NULL,             NULL,             NULL, 0, menuItemGenStringCurSelGun},
   { "Pellet",       NULL, &menu_top_level[2], NULL,             NULL,             NULL, 0, menuItemGenStringCurSelPellet},
-  { "Min. Return", menuItemGenStringSensitivity, &menu_top_level[3], menu_sensitivity, menu_sensitivity, NULL, 0, menuItemGenStringCurSelSensitivity},
-  { "Units",        NULL, &menu_top_level[4], menu_units,       menu_units,       NULL, 0, menuItemGenStringCurSelUnits},
-  { "Display Flip", NULL, &menu_top_level[5], menu_display_flip,menu_display_flip,NULL, 0, menuItemGenStringCurDisplayFlip},
-  { "Power Save",   NULL, &menu_top_level[6], menu_power_save, menu_power_save,  NULL, 0, menuItemGenStringCurPowerSaving},
+  { "Shot String",  NULL, &menu_top_level[3], menu_shot_string, menu_shot_string, NULL, 0, menuItemGenStringCurShotString},
+  { "Min. Return", menuItemGenStringSensitivity, &menu_top_level[4], menu_sensitivity, menu_sensitivity, NULL, 0, menuItemGenStringCurSelSensitivity},
+  { "Units",        NULL, &menu_top_level[5], menu_units,       menu_units,       NULL, 0, menuItemGenStringCurSelUnits},
+  { "Display Flip", NULL, &menu_top_level[6], menu_display_flip,menu_display_flip,NULL, 0, menuItemGenStringCurDisplayFlip},
+  { "Power Save",   NULL, &menu_top_level[7], menu_power_save, menu_power_save,  NULL, 0, menuItemGenStringCurPowerSaving},
   { "Sleep",        NULL, &menu_top_level[0], menu_sleep,      menu_sleep,        NULL, 0, menuItemGenStringCurSleep}
   
 };
@@ -858,18 +1053,34 @@ void doubleClick()
 void longPressStop()
 {
   if(renderMenu) {
-    if(menuStackIndex == 1) {
+    if(state == STATE_OFFLINE) {
       renderMenu = false;
       tft.fillScreen(TFT_BLACK);
-      if(profile_changed) {
-        pClient->disconnect();
-        state = STATE_IDLE;
+      state = STATE_IDLE;
+    }else { 
+      if(menuStackIndex == 1) {
+        renderMenu = false;
+        tft.fillScreen(TFT_BLACK);
+        if(profile_changed) {
+          Serial.println("P changed\n");
+          ESP.restart();
+          while(1){}
+        }
+      } else {
+        dirty = true;
+        pCurrentMenuItem = menuStack[--menuStackIndex];
       }
-    } else {
-      dirty = true;
-      pCurrentMenuItem = menuStack[--menuStackIndex];
     }
   } else if(state == STATE_CONNECTED) {
+    profile_changed = false;
+    dirty = true;
+    renderMenu = true;
+    pCurrentMenuItem = &menu_entry;
+    menu_entry.currentSubMenu = menu_top_level;
+    menuStackIndex = 0;
+    menuStack[menuStackIndex++] = pCurrentMenuItem;
+  } else if(state == STATE_IDLE) {
+    state = STATE_OFFLINE;
     profile_changed = false;
     dirty = true;
     renderMenu = true;
